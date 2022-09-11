284e2f97fb517b0fff21b13d06a10898a08bc18c
gsfranzoni <gsfranzoni@gmail.com>
2022-09-11 12:57:33 -0300

    feat: first commit

---
+++ src/main/java/com/gsfranzoni/springawsecsdeploy/SpringAwsEcsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.gsfranzoni.springawsecsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsEcsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsEcsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsfranzoni/springawsecsdeploy/SpringAwsEcsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsfranzoni.springawsecsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsEcsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
